6ddcfd3e6d0b96e6e6d046b06c612ede63ef9b07
Alex Sickler <sicklera@chop.edu>
2023-09-28 10:08:34 -0400

    :temp: add temporary debugging message

--- query_functions.py
+++ query_functions.py
@@ -372,6 +372,8 @@ def get_all_studies(client):
     # run query
     result = client.execute(query)
 
+    print(result)
+
     for org_edge in result["viewer"]["organizationUsers"]["edges"]:
         for study_edge in org_edge["node"]["organization"]["studies"]["edges"]:
             study = study_edge["node"]

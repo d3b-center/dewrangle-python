6cd7d83e4363ccd028cf14ae132706c63b3e08d0
Alex Sickler <sicklera@chop.edu>
2023-10-11 13:37:44 -0400

    :recycle: remove accidentally added file

--- download_job_result.py
+++ download_job_result.py
@@ -3,7 +3,6 @@ import sys
 import argparse
 import requests
 import credential
-import pandas as pd
 import query_functions as qf
 
 
@@ -30,22 +29,6 @@ def parse_args(args):
     return job, out
 
 
-def request_to_df(url, **kwargs):
-    """Call api and return response as a pandas dataframe."""
-    my_data = []
-    with requests.get(url, **kwargs) as response:
-        # check if the request was successful
-        if response.status_code == 200:
-            for line in response.iter_lines():
-                my_data.append(line.decode().split(","))
-        else:
-            print(f"Failed to fetch the CSV. Status code: {response.status_code}")
-
-    my_cols = my_data.pop(0)
-    df = pd.DataFrame(my_data, columns=my_cols)
-    return df
-
-
 def main(args):
     """Main, take args, run script."""
     job_id, out_base = parse_args(args)
@@ -61,10 +44,17 @@ def main(args):
 
     url = endpoint + job_id + "/result"
 
-    df = request_to_df(url, headers=req_header, stream=True)
+    response = requests.get(url, headers=req_header)
 
-    df.to_csv(out_file, index=False)
-
+    with requests.get(url, headers=req_header, stream=True) as response:
+    # check if the request was successful
+        if response.status_code == 200:
+            with open(out_file, "wb") as f:
+                for chunk in response.iter_content(chunk_size=8192):  # 8K chunks
+                    f.write(chunk)
+            print("CSV file downloaded successfully!")
+        else:
+            print(f"Failed to fetch the CSV. Status code: {response.status_code}")
 
 if __name__ == "__main__":
     # execute only if run as a script
